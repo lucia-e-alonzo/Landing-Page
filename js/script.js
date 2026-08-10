0f5a113938d90e8dfa94d998db4bd575671302f1
Raúl Molina <crsuruy@correo.url.edu.gt>
2026-08-10 15:12:35 -0600

    Primera version para la cronología interactiva (HU-3)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            }
+        });
+    }, {
+        threshold: 0.15 
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
